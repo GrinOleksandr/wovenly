f134b6b150ea8172a67a71bff21d92f2c11c0c08
GrinOleksandr <sasha.lisove@gmail.com>
2019-03-27 15:38:09 +0200

    nodemon works

--- server/app.js
+++ server/app.js
@@ -2,21 +2,17 @@ const express = require("express"),
     app = express(),
     config = require('./config');
 
-app.set("view engine", "ejs");
+// Serve static files
+app.use(express.static(path.join(__dirname, '../public')));
 
-let props = {
-    title: "Мои контакты",
-    emailsVisible: true,
-    emails: ["gavgav@mycorp.com", "mioaw@mycorp.com"],
-    phone: "+1234567890"
-};
-
-app.use("/", function(request, response){
-    response.render("contact", props);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'../public'));
 });
-
-
 
 
 app.listen(config.port,config.ip);
 console.log(`*****Server running at ${config.ip}:${config.port}`);
+
+
+
+
