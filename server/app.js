db4825ff66a046586e1d577614ead5ad73b44b99
GrinOleksandr <sasha.lisove@gmail.com>
2019-03-27 17:01:32 +0200

    file structure modified

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require("express"),
     app = express(),
-    config = require('./config');
+    config = require('./config'),
+    path = require("path");
+
 
 // Serve static files
 app.use(express.static(path.join(__dirname, '../public')));
@@ -8,7 +10,6 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'../public'));
 });
-
 
 app.listen(config.port,config.ip);
 console.log(`*****Server running at ${config.ip}:${config.port}`);
