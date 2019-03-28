7daa2b2dfe8f6b4310a5ad2b032797a1c000badd
GrinOleksandr <sasha.lisove@gmail.com>
2019-03-28 12:30:47 +0200

    heroku setup

--- server/app.js
+++ server/app.js
@@ -5,14 +5,14 @@ const express = require('express'),
   db = require('./DB/db.json')
 
 // Serve static files
-app.use(express.static(path.join(process.env.PWD, '../public')))
+app.use(express.static(path.join(process.env.PWD, '/public')))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(process.env.PWD + '../public/index.html'))
+  res.sendFile(path.join(process.env.PWD + '/public/index.html'))
 })
 
 app.listen(config.port, config.ip)
-console.log(path.join(process.env.PWD + '../public'))
+console.log(path.join(process.env.PWD + '/public'))
 console.log(process.env.PWD)
 console.log(process.cwd())
 console.log(`*****Server running at localhost ${config.port}`)
