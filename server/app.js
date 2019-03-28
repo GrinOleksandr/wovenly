b3054e65cea2b2de25428aeb66abd7b7f17a702b
GrinOleksandr <sasha.lisove@gmail.com>
2019-03-28 12:09:14 +0200

    heroku setup

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const express = require('express'),
 app.use(express.static(path.join(__dirname, './../public')))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + './../public'))
+  res.sendFile(path.join(__dirname + './../public/index.html'))
 })
 
 app.listen(config.port, config.ip)
