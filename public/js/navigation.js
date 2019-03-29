fc0654014693310e25ae04b0c220d10c9c195372
GrinOleksandr <sasha.lisove@gmail.com>
2019-03-29 16:19:20 +0200

    burger dropdown, and navmenu-link dropdown is now working

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -6,3 +6,9 @@ moreStylesBtn.addEventListener('click', function (ev) {
   console.log(moreStylesMenu.style.display)
   moreStylesMenu.classList.toggle('visible')
 })
+
+let navMenu = document.getElementById('navMenu')
+let burgerBtn = document.getElementById('burger')
+burgerBtn.addEventListener('click', function () {
+  navMenu.classList.toggle('visible')
+})
