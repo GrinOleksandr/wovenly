dbc1d56788278d8d82e95ca12bda2245aa88483e
GrinOleksandr <sasha.lisove@gmail.com>
2019-03-29 17:58:20 +0200

    search field added

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,13 +1,18 @@
 let moreStylesMenu = document.getElementById('moreStylesMenu')
 let moreStylesBtn = document.getElementById('moreStylesBtn')
-moreStylesBtn.addEventListener('click', function (ev) {
-  console.log(ev)
-  console.log(moreStylesMenu.style.display)
-  moreStylesMenu.classList.toggle('visible')
+moreStylesBtn.addEventListener('click', function () {
+  moreStylesMenu.classList.toggle('visible');
 })
 
 let navMenu = document.getElementById('navMenu')
 let burgerBtn = document.getElementById('burger')
 burgerBtn.addEventListener('click', function () {
-  navMenu.classList.toggle('visible')
+  navMenu.classList.toggle('visible');
+})
+
+let searchFieldWrapper = document.getElementById('searchFieldWrapper')
+let searchBtn = document.getElementById('searchButton')
+searchBtn.addEventListener('click', function (ev) {
+  ev.stopPropagation()
+  searchFieldWrapper.classList.toggle('visible');
 })
