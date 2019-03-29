3571a613f20c25b248e23fab6c399818e3c8da18
GrinOleksandr <sasha.lisove@gmail.com>
2019-03-29 16:28:46 +0200

    project setup finished

---
+++ public/js/navigation.js
@@ -0,0 +1,14 @@
+// more-styles dropDown apearance
+let moreStylesMenu = document.getElementById('moreStylesMenu')
+let moreStylesBtn = document.getElementById('moreStylesBtn')
+moreStylesBtn.addEventListener('click', function (ev) {
+  console.log(ev)
+  console.log(moreStylesMenu.style.display)
+  moreStylesMenu.classList.toggle('visible')
+})
+
+let navMenu = document.getElementById('navMenu')
+let burgerBtn = document.getElementById('burger')
+burgerBtn.addEventListener('click', function () {
+  navMenu.classList.toggle('visible')
+})
