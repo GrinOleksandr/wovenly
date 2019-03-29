b3061abc7c6b4cfa324aea5a046bc5a23c33e7b2
GrinOleksandr <sasha.lisove@gmail.com>
2019-03-29 16:27:28 +0200

    project setup finished

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,4 +1,4 @@
-// more-styles dropDown apearance
+// more-styles dropDown apearance.
 let moreStylesMenu = document.getElementById('moreStylesMenu')
 let moreStylesBtn = document.getElementById('moreStylesBtn')
 moreStylesBtn.addEventListener('click', function (ev) {
