8f365b80b4d124cdd9cd17302b27cef4ac5889d8
GrinOleksandr <sasha.lisove@gmail.com>
2019-04-01 17:17:42 +0300

    navigation almost tested

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -2,34 +2,35 @@ let moreStylesMenu = document.getElementById('moreStylesMenu')
 let moreStylesBtn = document.getElementById('moreStylesBtn')
 moreStylesBtn.addEventListener('click', moreStylesHandler)
 moreStylesBtn.nextSibling.addEventListener('click', moreStylesHandler)
-function moreStylesHandler () {
+
+function moreStylesHandler() {
   moreStylesMenu.classList.toggle('dropdown__visible')
   if (searchFieldWrapper.classList.contains('dropdown__visible')) {
     searchFieldWrapper.classList.remove('dropdown__visible');
-}
+  }
 }
 
 let navMenu = document.getElementById('navMenu')
 let burgerBtn = document.getElementById('burger')
-burgerBtn.addEventListener('click', function () {
+burgerBtn.addEventListener('click', function() {
   navMenu.classList.toggle('dropdown__visible')
   if (searchFieldWrapper.classList.contains('dropdown__visible')) {
     searchFieldWrapper.classList.remove('dropdown__visible');
-}
+  }
 })
 
 let searchFieldWrapper = document.getElementById('searchFieldWrapper')
 let searchBtn = document.getElementById('searchButton')
-searchBtn.addEventListener('click', function (ev) {
+searchBtn.addEventListener('click', function(ev) {
   ev.stopPropagation()
   searchFieldWrapper.classList.toggle('dropdown__visible')
   if (navMenu.classList.contains('dropdown__visible')) {
     navMenu.classList.remove('dropdown__visible');
-}
+  }
 
-if (moreStylesMenu.classList.contains('dropdown__visible')) {
-  moreStylesMenu.classList.remove('dropdown__visible');
-}
+  if (moreStylesMenu.classList.contains('dropdown__visible')) {
+    moreStylesMenu.classList.remove('dropdown__visible');
+  }
 })
 
 let cartCount = document.getElementById('cartCount')
@@ -37,12 +38,12 @@ let cart = document.getElementById('cart')
 cart.addEventListener('click', cartCounterIncrement)
 cartCount.addEventListener('click', cartCounterIncrement)
 
-function cartCounterIncrement () {
+function cartCounterIncrement() {
   console.log(cartCount.style.display)
   if (!cartCount.classList.contains('visible')) {
     cartCount.classList.add('visible')
     console.log(cartCount.classList.add('visible'));
-}
+  }
 
-cartCount.innerText++;
+  cartCount.innerText++;
 }
