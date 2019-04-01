82fa6648b375a494a7226a9858ebd3edee3ac158
GrinOleksandr <sasha.lisove@gmail.com>
2019-04-01 10:29:36 +0300

    fixing nav menu style and script

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,18 +1,31 @@
 let moreStylesMenu = document.getElementById('moreStylesMenu')
 let moreStylesBtn = document.getElementById('moreStylesBtn')
 moreStylesBtn.addEventListener('click', function () {
-  moreStylesMenu.classList.toggle('visible');
+  moreStylesMenu.classList.toggle('dropdown__visible')
+  if (searchFieldWrapper.classList.contains('dropdown__visible')) {
+    searchFieldWrapper.classList.remove('dropdown__visible')
+  }
 })
 
 let navMenu = document.getElementById('navMenu')
 let burgerBtn = document.getElementById('burger')
 burgerBtn.addEventListener('click', function () {
-  navMenu.classList.toggle('visible');
+  navMenu.classList.toggle('dropdown__visible')
+  if (searchFieldWrapper.classList.contains('dropdown__visible')) {
+    searchFieldWrapper.classList.remove('dropdown__visible')
+  }
 })
 
 let searchFieldWrapper = document.getElementById('searchFieldWrapper')
 let searchBtn = document.getElementById('searchButton')
 searchBtn.addEventListener('click', function (ev) {
   ev.stopPropagation()
-  searchFieldWrapper.classList.toggle('visible');
+  searchFieldWrapper.classList.toggle('dropdown__visible')
+  if (navMenu.classList.contains('dropdown__visible')) {
+    navMenu.classList.remove('dropdown__visible')
+  }
+
+  if (moreStylesMenu.classList.contains('dropdown__visible')) {
+    moreStylesMenu.classList.remove('dropdown__visible')
+  }
 })
