5a04bc86d17dbd164571a083a1fa2ef24267e056
GrinOleksandr <sasha.lisove@gmail.com>
2019-04-03 15:48:41 +0300

    header-content block finished

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -3,34 +3,37 @@ let moreStylesBtn = document.getElementById('moreStylesBtn')
 moreStylesBtn.addEventListener('click', moreStylesHandler)
 moreStylesBtn.nextSibling.addEventListener('click', moreStylesHandler)
 
-function moreStylesHandler() {
+function moreStylesHandler(ev) {
+  ev.preventDefault();
   moreStylesMenu.classList.toggle('dropdown__visible')
   if (searchFieldWrapper.classList.contains('dropdown__visible')) {
     searchFieldWrapper.classList.remove('dropdown__visible');
-  }
+}
 }
 
 let navMenu = document.getElementById('navMenu')
 let burgerBtn = document.getElementById('burger')
-burgerBtn.addEventListener('click', function() {
+burgerBtn.addEventListener('click', function(ev) {
+  ev.preventDefault();
   navMenu.classList.toggle('dropdown__visible')
   if (searchFieldWrapper.classList.contains('dropdown__visible')) {
     searchFieldWrapper.classList.remove('dropdown__visible');
-  }
+}
 })
 
 let searchFieldWrapper = document.getElementById('searchFieldWrapper')
 let searchBtn = document.getElementById('searchButton')
 searchBtn.addEventListener('click', function(ev) {
+  ev.preventDefault();
   ev.stopPropagation()
   searchFieldWrapper.classList.toggle('dropdown__visible')
   if (navMenu.classList.contains('dropdown__visible')) {
     navMenu.classList.remove('dropdown__visible');
-  }
+}
 
-  if (moreStylesMenu.classList.contains('dropdown__visible')) {
-    moreStylesMenu.classList.remove('dropdown__visible');
-  }
+if (moreStylesMenu.classList.contains('dropdown__visible')) {
+  moreStylesMenu.classList.remove('dropdown__visible');
+}
 })
 
 let cartCount = document.getElementById('cartCount')
@@ -38,12 +41,11 @@ let cart = document.getElementById('cart')
 cart.addEventListener('click', cartCounterIncrement)
 cartCount.addEventListener('click', cartCounterIncrement)
 
-function cartCounterIncrement() {
-  console.log(cartCount.style.display)
+function cartCounterIncrement(ev) {
+  ev.preventDefault();
   if (!cartCount.classList.contains('visible')) {
-    cartCount.classList.add('visible')
-    console.log(cartCount.classList.add('visible'));
-  }
+    cartCount.classList.add('visible');
+}
 
-  cartCount.innerText++;
+cartCount.innerText++;
 }
