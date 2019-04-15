296e3280d714bed3e467467b47aa64776e5e9b87
GrinOleksandr <sasha.lisove@gmail.com>
2019-04-15 14:42:13 +0300

    implementing slider

--- src/sliders.js
+++ src/sliders.js
@@ -1,7 +1,33 @@
-
-
-$(document).ready(function(){
-  $('.your-class').slick({
-    f: setting-value
+console.log('slider initialized')
+$(document)
+  .ready(function() {
+    $('.popular__slider--wrapper')
+      .slick({
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        easing: 'swing',
+        speed: 1200,
+        dots: false,
+        prevArrow: $('.popular__prev'),
+        nextArrow: $('.popular__next'),
+        responsive: [{
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 4,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              infinite: true,
+              dots: true
+            }
+          },
+        ]
+      });
   });
-});
