21db6da199c54daa847626b1c26fb27af960a794
GrinOleksandr <sasha.lisove@gmail.com>
2019-04-18 13:50:01 +0300

    implementing EJS and data handler

--- src/discoverYourStyle.js
+++ src/discoverYourStyle.js
@@ -1,80 +1,85 @@
-// fetch(`https://wovenly-server.herokuapp.com/getpopular`, {
-//     method: 'GET',
-//     headers: {
-//       'Content-Type': 'text/plain',
-//       'Access-Control-Allow-Origin': "*"
-//     }
-//   })
-//   .then(function(response) {
-//     return response.json()
-//   })
-//   .then(function(json) {
-//     console.log(json);
-//     let popularProductTemplate = new EJS({
-//       url: './templates/popular.ejs'
-//     });
-//     $(".popular__slider__nesting-place")
-//       .html(popularProductTemplate.render({
-//         products: json
-//       }));
-//   })
-//   .then(function() {
-//     $('.popular__slider--wrapper')
-//       .slick({
-//         slidesToShow: 6,
-//         slidesToScroll: 6,
-//         easing: 'swing',
-//         speed: 1200,
-//         swipe: false,
-//         prevArrow: $('.popular__prev'),
-//         nextArrow: $('.popular__next'),
-//         responsive: [{
-//             breakpoint: 1200,
-//             settings: {
-//               slidesToShow: 4,
-//               slidesToScroll: 4,
-//               infinite: true,
-//             }
-//           },
-//           {
-//             breakpoint: 768,
-//             settings: {
-//               slidesToShow: 2,
-//               slidesToScroll: 2,
-//               infinite: true,
-//             }
-//           },
-//         ]
-//       });
-//
-//   })
-//   .catch(error => error);
+fetch(`https://wovenly-server.herokuapp.com/getstyles`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'text/plain',
+      'Access-Control-Allow-Origin': "*"
+    }
+  })
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(json) {
+    console.log(json);
+    let discoverYourStyleTemplate = new EJS({
+      url: './templates/discoverYourStyle.ejs'
+    });
+    $(".discover-your-style__slider--nesting-place")
+      .html(discoverYourStyleTemplate.render({
+        incomingStyles: json
+      }));
+  })
+  .then(function() {
+    $('.discover-your-style__wrapper')
+      .slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        easing: 'swing',
+        speed: 1200,
+        swipe: false,
+        prevArrow: $('.discover-your-style__prev'),
+        nextArrow: $('.discover-your-style__next'),
+        responsive: [{
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              infinite: true,
+            }
+          },
+        ]
+      });
+  })
+  .catch(error => error);
 
 
-$('.discover-your-style__wrapper')
-  .slick({
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    easing: 'swing',
-    speed: 1200,
-    swipe: false,
-    prevArrow: $('.discover-your-style__prev'),
-    nextArrow: $('.discover-your-style__next'),
-    responsive: [{
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-        }
-      },
-    ]
-  });
+
+
+
+
+// ---------------
+
+// $('.discover-your-style__wrapper')
+//   .slick({
+//     slidesToShow: 4,
+//     slidesToScroll: 4,
+//     easing: 'swing',
+//     speed: 1200,
+//     swipe: false,
+//     prevArrow: $('.discover-your-style__prev'),
+//     nextArrow: $('.discover-your-style__next'),
+//     responsive: [{
+//         breakpoint: 1200,
+//         settings: {
+//           slidesToShow: 3,
+//           slidesToScroll: 3,
+//           infinite: true,
+//         }
+//       },
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           slidesToShow: 2,
+//           slidesToScroll: 2,
+//           infinite: true,
+//         }
+//       },
+//     ]
+//   });
