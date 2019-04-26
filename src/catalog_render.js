5409c3670712eecffbb7f99493b364f55df59906
GrinOleksandr <sasha.lisove@gmail.com>
2019-04-26 12:29:23 +0300

    template works

--- src/catalog_render.js
+++ src/catalog_render.js
@@ -1,10 +1,12 @@
 getCatalogData()
   .then(function(data) {
+  debugger;
     let catalogItemTemplate = new EJS({
       url: './templates/product_card.ejs'
     });
-    $(".catalog__content")
-      .html(catalogItemTemplate.render({
+      console.log('catalog templates initialised in:  ', $(".catalog__content"));
+    
+    $(".catalog__content").html(catalogItemTemplate.render({
         block: "catalog",
         products: data
       }));
@@ -13,8 +15,8 @@ getCatalogData()
 
   function getCatalogData() {
     return new Promise(function(resolve, reject) {
-      let data = getLocalStorageObjectItem('homepageData')
-        .popular;
+      let data = getLocalStorageObjectItem('homepageData').popular;
+      console.log("catalog data readed!")
       resolve(data)
     })
   }
