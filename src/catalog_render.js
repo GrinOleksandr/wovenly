6cc1af4ed868df9bed6f8caa715ed48dce15b2c2
GrinOleksandr <sasha.lisove@gmail.com>
2019-04-26 13:50:31 +0300

    catalog content-header style implemented, catalog content style implemented

--- src/catalog_render.js
+++ src/catalog_render.js
@@ -1,7 +1,6 @@
 getCatalogData()
   .then(function(data) {
-  debugger;
-    let catalogItemTemplate = new EJS({
+  let catalogItemTemplate = new EJS({
       url: './templates/product_card.ejs'
     });
       console.log('catalog templates initialised in:  ', $(".catalog__content"));
@@ -11,6 +10,13 @@ getCatalogData()
         products: data
       }));
   })
+  .then(function() {
+      implementActivation();
+      $('.product__color--outer:first-child')
+        .each(function(index, element) {
+          activateColor(element);
+        })
+    })
   .catch(error => error);
 
   function getCatalogData() {
@@ -28,3 +34,51 @@ getCatalogData()
       }
       return JSON.parse(json);
   }
+
+//color activator
+  const throttle = (func, limit) => {
+    let lastFunc
+    let lastRan
+    return function() {
+      const context = this
+      const args = arguments
+      if (!lastRan) {
+        func.apply(context, args)
+        lastRan = Date.now()
+      } else {
+        clearTimeout(lastFunc)
+        lastFunc = setTimeout(function() {
+          if ((Date.now() - lastRan) >= limit) {
+            func.apply(context, args)
+            lastRan = Date.now()
+          }
+        }, limit - (Date.now() - lastRan))
+      }
+    }
+  }
+  
+  function activateColor(element) {
+    if ($(element).parent().find('.product__color--active')) {
+      $(element).parent().find('.product__color--active')
+        .removeClass('product__color--active');
+    }
+  
+    $(element).css('--activeColor-color', $(element).css('background-color'));
+    $(element).addClass('product__color--active');
+    element.parentNode.parentNode.dataset.activeColor = element.dataset.color;
+  }
+  
+  function implementActivation() {
+    $('.product__color--outer')
+      .each(function(index, element) {
+        $(element)
+          .on('click', function(ev) {
+            activateColor(ev.target)
+          })
+      })
+  }
+  window.addEventListener('resize', throttle(implementActivation, 1000))
+  
+
+
+
