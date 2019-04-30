8bc8e8f5ef0324849038ff38cee9080d7f84c5be
GrinOleksandr <sasha.lisove@gmail.com>
2019-04-30 16:02:42 +0300

    category expansion on mobile implemented

--- src/catalog_FiltersView.js
+++ src/catalog_FiltersView.js
@@ -5,12 +5,14 @@
 
 $('.more-filters__category--expander').click(function(ev){
   let thisCategory = $(ev.target).parent().parent();
-  if(thisCategory.find(".filter__category--filters").css("visibility","hidden")){
+  if(thisCategory.find(".filter__category--filters").css("visibility")==="hidden"){
     thisCategory.find(".filter__category--filters").css("visibility","visible");
-  }
-  else{
-      thisCategory.find(".filter__category--filters").css("visibility","hidden")
-  }
+    $(ev.target).html("⎯").css("padding-top","4px");
     
-  
+  }
+  else {
+      thisCategory.find(".filter__category--filters").css("visibility","hidden");
+      $(ev.target).html("+").css("padding-top","10px");
+  }
+    console.log('category expanded')
 })
