7cdf1ccc380bf819d4c91c75a809eda171e3b4b3
GrinOleksandr <sasha.lisove@gmail.com>
2019-04-30 17:13:47 +0300

    implementing activate category on mobile

--- src/catalog_FiltersView.js
+++ src/catalog_FiltersView.js
@@ -1,5 +1,5 @@
 
-//Expand category in mobile mode
+//Expand category "MORE FILTERS" mode
 $('.more-filters__category--expander').click(function(ev){
   let thisCategory = $(ev.target).parent().parent();
   if(thisCategory.find(".filter__category--filters").css("visibility")==="hidden"){
@@ -13,19 +13,41 @@ $('.more-filters__category--expander').click(function(ev){
   }
 })
 
+//Expand category
+$(".filter__category--button").click(function(ev){
+  let thisCategory = $(ev.target).parent().parent();
+  if(thisCategory.find(".filter__category--filters").css("visibility")==="hidden"){
+    thisCategory.find(".filter__category--filters").css("visibility","visible");
+    $(ev.target).html("⎯").css("padding-top","4px");
+    
+  }
+  else {
+      thisCategory.find(".filter__category--filters").css("visibility","hidden");
+      $(ev.target).html("+").css("padding-top","10px");
+  }
+})
 
+
+
+
+
+
+
+
+//select filter
 $('.filter').click(function(ev){
   selectFilter(ev.target)
 })
 
 function selectFilter(filter){
+  let allreadySelected
   if(filter.parentElement.dataset.selectedfilters){
-    let allreadySelected = filter.parentElement.dataset.selectedfilters.split(",");
+    allreadySelected = filter.parentElement.dataset.selectedfilters.split(",");
   }
   else{
-    let allreadySelected = [];
+  allreadySelected = [];
+  }
     if(!$(filter).hasClass("filter--selected")){
-      console.log(allreadySelected)
       allreadySelected.push($(filter).text());
       $(filter).toggleClass("filter--selected");
       }
@@ -34,11 +56,8 @@ function selectFilter(filter){
       $(filter).toggleClass("filter--selected");
     }
       $(filter).parent().attr("data-selectedfilters", allreadySelected)
-    
-    
-  }
-
-  
-  
+      console.log("Current selected filters in this Cat.: ", allreadySelected)
 }
+
+
 
