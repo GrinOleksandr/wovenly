8b4db2343d156730d81f425037714562411ab888
GrinOleksandr <sasha.lisove@gmail.com>
2019-05-03 10:02:26 +0300

    task for filters menut is temporary closed

--- src/catalog_FiltersView.js
+++ src/catalog_FiltersView.js
@@ -1,69 +1,69 @@
-
 //Expand category "MORE FILTERS" mode
-$('.more-filters__category--expander').click(function(ev){
+$('.more-filters__category--expander').click(function(ev) {
+  ev.preventDefault();
+  ev.stopPropagation();
+
   let thisCategory = $(ev.target).parent().parent();
-  if($(ev.target).hasClass('more-filters__button--closed')){
+  if ($(ev.target).hasClass('more-filters__button--closed')) {
+    console.log("expanding", thisCategory)
     collapseFilterCategory();
     thisCategory.addClass('filter__category--expanded');
-    $(ev.target).html("⎯").removeClass('more-filters__button--closed').addClass('more-filters__button--opened');
-    $(ev.target).parent().parent().css("border-bottom","none");
-}
-  else {
+    $(ev.target).html("⎯").removeClass('more-filters__button--closed');
+    $(ev.target).addClass('more-filters__button--opened');
+  } else {
     collapseFilterCategory();
-      $(ev.target).html("+").removeClass('more-filters__button--opened').addClass('more-filters__button--closed');
-      $(ev.target).parent().parent().css("border-bottom","1px solid #b3b3b3");
+    console.log($(ev.target))
+    $(ev.target).removeClass('more-filters__button--opened');
+    $(ev.target).addClass('more-filters__button--closed').html("+");
   }
 })
 
 //Expand category
-$(".filter__category--button").click(function(ev){
-  console.log("Expanding category: ",$(ev.target).text());
-  if($(ev.target).parent().hasClass("filter__category--active")){
-  deactivateFilterCategory();
-}
-else {
-  deactivateFilterCategory();
+$(".filter__category--button").click(function(ev) {
+  console.log("Expanding category: ", $(ev.target).text());
+  if ($(ev.target).parent().hasClass("filter__category--active")) {
+    deactivateFilterCategory();
+  } else {
+    deactivateFilterCategory();
     $(ev.target).parent().toggleClass("filter__category--active");
-    $('.overlay__layer').css("display","block");
-    $('.filters').css("position","absolute");
-}
+    $('.overlay__layer').css("display", "block");
+    $('.filters').css("position", "absolute");
+  }
 })
 
 //deactivate filter category
-function deactivateFilterCategory(){
-    $(".filter__category--active").removeClass('filter__category--active');
-    $('.overlay__layer').css("display","none");
-    $('.filters').css("position","relative");
+function deactivateFilterCategory() {
+  $(".filter__category--active").removeClass('filter__category--active');
+  $('.overlay__layer').css("display", "none");
+  $('.filters').css("position", "relative");
 }
 //collapse filter category(more filters mode)
-function collapseFilterCategory(){
-    $(".filter__category--expanded").removeClass('filter__category--expanded').find('.more-filters__category--expander').removeClass('more-filters__button--closed').addClass('more-filters__button--opened').html("+");
+function collapseFilterCategory() {
+  console.log("expander!!! "  ,$(".filter__category--expanded").find('.more-filters__category--expander'))
+  $(".filter__category--expanded").find('.more-filters__category--expander').removeClass('more-filters__button--closed');
+  $(".filter__category--expanded").removeClass('filter__category--expanded');
+  $(".filter__category--expanded").find('.more-filters__category--expander').addClass('more-filters__button--opened').html("+")
 }
 
 //select filter
-$('.filter').click(function(ev){
+$('.filter').click(function(ev) {
   selectFilter(ev.target)
 })
 
-function selectFilter(filter){
+function selectFilter(filter) {
   let alreadySelected
-  if(filter.parentElement.dataset.selectedfilters){
+  if (filter.parentElement.dataset.selectedfilters) {
     alreadySelected = filter.parentElement.dataset.selectedfilters.split(",");
+  } else {
+    alreadySelected = [];
   }
-  else{
-  alreadySelected = [];
+  if (!$(filter).hasClass("filter--selected")) {
+    alreadySelected.push($(filter).text());
+    $(filter).toggleClass("filter--selected");
+  } else {
+    alreadySelected.splice(alreadySelected.indexOf($(filter).text()));
+    $(filter).toggleClass("filter--selected");
   }
-    if(!$(filter).hasClass("filter--selected")){
-      alreadySelected.push($(filter).text());
-      $(filter).toggleClass("filter--selected");
-      }
-    else {
-      alreadySelected.splice(alreadySelected.indexOf($(filter).text()));
-      $(filter).toggleClass("filter--selected");
-    }
-      $(filter).parent().attr("data-selectedfilters", alreadySelected)
-      console.log("Current selected filters in this Cat.: ", alreadySelected)
+  $(filter).parent().attr("data-selectedfilters", alreadySelected)
+  console.log("Current selected filters in this Cat.: ", alreadySelected)
 }
-
-
-
